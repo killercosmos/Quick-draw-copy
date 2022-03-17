354051fdfc8af8cd8668d4c4991720857b634c1c
killercosmos <76935007+killercosmos@users.noreply.github.com>
2022-03-17 18:07:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+
+}
+
+function preload() {
+classifier = ml5.imageClassifier("DoodleNet");
+}
+
+
+function clearCanvas() {
+    background("white");
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0)
+
+    if(mouseIsPressed) {
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}   
+
+function classifyCanvas() {
+    classifier.classify(canvas,gotResult);
+}
+function gotResult(error,results) {
+
+
+    if(error) {
+        console.error(error);
+    }
+
+    console.log(results);
+
+    document.getElementById("lable").innerHTML = "Lable:" + results[0].label;
+    document.getElementById("confidence").innerHTML = "Confidience" + Math.round(results[0].confidence * 100) + "%";
+
+    var utterThis = new SpeechSynthesisUtterance(results[0].label);
+
+    synth.speak(utterThis);
+}
